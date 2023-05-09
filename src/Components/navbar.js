d3b88dc2d44da9225c939a0fe061636256621ed8
lrigs <lauramariganello@gmail.com>
2023-05-09 16:03:25 -0400

    added mobile navbar animations

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,66 +1,73 @@
 import "../Styles/navbar.scss";
-
-function handleMouseEnter(e) {
-  e.target.classList.add("hovered");
-}
-
-function handleMouseLeave(e) {
-  e.target.classList.remove("hovered");
-}
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleBurgerClick() {
+    setIsVisible((prev) => !prev);
+  }
+
+  function handleListClick() {
+    setIsVisible(false);
+  }
+
+  function handleMouseEnter(e) {
+    e.target.classList.add("hovered");
+  }
+
+  function handleMouseLeave(e) {
+    e.target.classList.remove("hovered");
+  }
+
   return (
     <div className="navbar">
-      <div class="mobileitems">
-        <a href="#" class="burger">
-          <span class="bar"></span>
-          <span class="bar"></span>
-          <span class="bar"></span>
+      <div className="mobileitems">
+        <a
+          href="#"
+          className={`burger ${isVisible ? "burger-icon open" : ""}`}
+          onClick={handleBurgerClick}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
         </a>
       </div>
 
-      <ul className="sections">
-        <li>
-          <a
-            href="#"
-            className="menu-item"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            TechLab
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="menu-item"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="menu-item"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            Contact
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="menu-item blog"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            Blog
-          </a>
-        </li>
-      </ul>
+      <div className={`navbar-links ${isVisible ? "visible" : ""}`}>
+        <ul className="sections">
+          <li onClick={handleListClick}>
+            <a
+              href="#"
+              className="menu-item"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              TechLab
+            </a>
+          </li>
+          <li onClick={handleListClick}>
+            <a
+              href="#"
+              className="menu-item"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              About
+            </a>
+          </li>
+          <li onClick={handleListClick}>
+            <a
+              href="#"
+              className="menu-item"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
