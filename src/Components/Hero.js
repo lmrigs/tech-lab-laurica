3fcafc6696bb4d87770bd2042fac65929966eda3
lrigs <lauramariganello@gmail.com>
2023-07-24 06:49:56 -0400

    removing spline animation due to issues

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,9 +1,9 @@
 import "../Styles/Hero.scss";
-import Spline from "@splinetool/react-spline";
-import { useState, useEffect } from "react";
-import Loader from "../Images/loading_bubbles.gif";
+// import Spline from "@splinetool/react-spline";
+// import { useState, useEffect } from "react";
+// import Loader from "../Images/loading_bubbles.gif";
 
-export function HeroText() {
+export default function Hero() {
   return (
     <div className="hero">
       <h1 className="hero-text-header">
@@ -18,51 +18,51 @@ export function HeroText() {
   );
 }
 
-export function Animation() {
-  const [canvasLoaded, setCanvasLoaded] = useState(false);
+// export function Animation() {
+//   const [canvasLoaded, setCanvasLoaded] = useState(false);
 
-  useEffect(() => {
-    const canvasElement = document.querySelector(".spline canvas");
-    if (canvasElement) {
-      const observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-          if (mutation.attributeName === "style") {
-            const { display, width, height } = mutation.target.style;
-            const isCanvasLoaded =
-              display === "block" && width === "100%" && height === "100%";
+//   useEffect(() => {
+//     const canvasElement = document.querySelector(".spline canvas");
+//     if (canvasElement) {
+//       const observer = new MutationObserver((mutations) => {
+//         mutations.forEach((mutation) => {
+//           if (mutation.attributeName === "style") {
+//             const { display, width, height } = mutation.target.style;
+//             const isCanvasLoaded =
+//               display === "block" && width === "100%" && height === "100%";
 
-            if (isCanvasLoaded) {
-              setCanvasLoaded(true);
-              observer.disconnect();
-            }
-          }
-        });
-      });
+//             if (isCanvasLoaded) {
+//               setCanvasLoaded(true);
+//               observer.disconnect();
+//             }
+//           }
+//         });
+//       });
 
-      observer.observe(canvasElement, { attributes: true });
-    }
-  }, []);
+//       observer.observe(canvasElement, { attributes: true });
+//     }
+//   }, []);
 
-  return (
-    <div className="loading-animation">
-      <div className={`loading-overlay ${canvasLoaded ? "hidden" : ""}`}>
-        <img src={Loader} alt="My Image" />
-      </div>
+//   return (
+//     <div className="loading-animation">
+//       <div className={`loading-overlay ${canvasLoaded ? "hidden" : ""}`}>
+//         <img src={Loader} alt="My Image" />
+//       </div>
 
-      {/* Your Spline component */}
-      <Spline
-        className="spline"
-        scene="https://prod.spline.design/eZG7X-ABMmstzH9n/scene.splinecode"
-      />
-    </div>
-  );
-}
+//       {/* Your Spline component */}
+//       <Spline
+//         className="spline"
+//         scene="https://prod.spline.design/eZG7X-ABMmstzH9n/scene.splinecode"
+//       />
+//     </div>
+//   );
+// }
 
-export default function Hero() {
-  return (
-    <div className="container">
-      <HeroText />
-      <Animation />
-    </div>
-  );
-}
+// export default function Hero() {
+//   return (
+//     <div className="container">
+//       <HeroText />
+//       <Animation />
+//     </div>
+//   );
+// }
